6663a530db5e0875fbf0d1f3b6b19ee19db7c4ea
Pavel Balashov <pavel.balashov1@gmail.com>
2024-07-20 23:08:36 +0200

    feat: add more experimental code to sse component

--- apps/app-angular-1/src/components/sse/sse.component.ts
+++ apps/app-angular-1/src/components/sse/sse.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal, OnDestroy, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,14 +8,77 @@ import { CommonModule } from '@angular/common';
   templateUrl: './sse.component.html',
   styleUrl: './sse.component.css',
 })
-export class SseComponent implements OnInit {
+export class SseComponent implements OnInit, OnDestroy {
   messages = signal<string[]>([]);
+  private messageCount = 0;
+  private maxMessages = 6;
+  private eventSource!: EventSource;
+  private readyStateNumber = signal<number>(0);
+  private readyStateName = computed(() =>
+    this.getReadyStateName(this.readyStateNumber()),
+  );
 
   ngOnInit(): void {
-    const eventSource = new EventSource('http://localhost:4200/api/sse');
-    eventSource.onmessage = (event: MessageEvent<string>) => {
+    this.eventSource = new EventSource('http://localhost:4200/api/sse');
+
+    this.readyStateNumber.set(this.eventSource.readyState);
+    console.log('EventSource not started', this.readyStateName());
+
+    // this.eventSource.addEventListener('notice', (event) => {
+    //   console.log('EventSource notice event', event);
+    // });
+
+    this.eventSource.onmessage = (event: MessageEvent<string>) => {
       const payload = JSON.parse(event.data) as { message: string };
       this.messages.set([...this.messages(), payload.message]);
+
+      this.messageCount += 1;
+      if (this.messageCount >= this.maxMessages) {
+        this.closeEventSource();
+        this.readyStateNumber.set(this.eventSource.readyState);
+        console.log('EventSource closed', this.readyStateName());
+      }
     };
+
+    this.eventSource.onopen = (event) => {
+      this.readyStateNumber.set(this.eventSource.readyState);
+      console.log(
+        'EventSource onopen event',
+        this.eventSource.url,
+        this.readyStateName(),
+        event,
+      );
+    };
+
+    this.eventSource.onerror = (event) => {
+      this.readyStateNumber.set(this.eventSource.readyState);
+      console.log('EventSource onerror event', this.readyStateName(), event);
+      // Client will automatically try to reconnect if the connection is not closed
+      this.closeEventSource();
+    };
+  }
+
+  ngOnDestroy(): void {
+    this.eventSource.close();
+  }
+
+  private closeEventSource(): void {
+    // onerror is not triggered when the client closes the connection
+    this.eventSource.close();
+    this.readyStateNumber.set(this.eventSource.readyState);
+    console.log('EventSource closed', this.readyStateName());
+  }
+
+  private getReadyStateName(readyStateNumber: number): string {
+    switch (readyStateNumber) {
+      case EventSource.CONNECTING:
+        return 'CONNECTING';
+      case EventSource.OPEN:
+        return 'OPEN';
+      case EventSource.CLOSED:
+        return 'CLOSED';
+      default:
+        return 'UNKNOWN';
+    }
   }
 }
